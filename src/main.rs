f49f32df930c99ea7ef40b3662c6b3bfc44fb525
Daniel Basedow <db@connectedcars.dk>
2019-07-12 20:34:09 +0200

    improve UTCTime handling

--- src/main.rs
+++ src/main.rs
@@ -175,16 +175,79 @@ impl<'a> Debug for Utf8String<'a> {
 struct UTCTime<'a>(&'a [u8]);
 
 impl<'a> UTCTime<'a> {
-    fn to_datetime(&self) -> Result<DateTime<Utc>, Error> {
-        //TODO implement
+    fn to_datetime(&self) -> Result<DateTime<FixedOffset>, Error> {
+        let data = self.0;
+        let year = ascii_slice_to_u32(&data[..2])?;
+        let year = year + 2000;
+
+        let month = ascii_slice_to_u32(&data[2..4])?;
+        let day = ascii_slice_to_u32(&data[4..6])?;
+
+        let hour = ascii_slice_to_u32(&data[6..8])?;
+        let minute = ascii_slice_to_u32(&data[8..10])?;
+
+        let second;
+        if data.len() == 13 || data.len() == 17 {
+            second = ascii_slice_to_u32(&data[10..12])?;
+        } else {
+            second = 0;
+        }
+
+        let utc_offset: i32;
+        if data.len() == 17 {
+            let hour_offset = ascii_slice_to_u32(&data[11..13])?;
+            let minute_offset = ascii_slice_to_u32(&data[14..16])?;
+            let factor = if data[10] == 0x2d { // '-'
+                -1
+            } else { 1 };
+
+            utc_offset = factor * (hour_offset * 3600 + minute_offset * 60) as i32;
+        } else if data.len() == 19 {
+            let hour_offset = ascii_slice_to_u32(&data[13..15])?;
+            let minute_offset = ascii_slice_to_u32(&data[16..18])?;
+            let factor = if data[12] == 0x2d { // '-'
+                -1
+            } else { 1 };
+
+            utc_offset = factor * (hour_offset * 3600 + minute_offset * 60) as i32;
+        } else {
+            utc_offset = 0;
+        }
+
+        let offset = FixedOffset::east(utc_offset);
+        let dt = offset.ymd(year as i32, month, day).and_hms(hour, minute, second);
+
+        Ok(dt)
+    }
+}
+
+fn ascii_to_digit(d: u8) -> Result<u32, Error> {
+    if d >= 0x30 && d <= 0x39 {
+        Ok((d & 0x0f) as u32)
+    } else {
         Err(Error::ParseError)
     }
 }
 
+fn ascii_slice_to_u32(data: &[u8]) -> Result<u32, Error> {
+    let mut res: u32 = 0;
+    for &ch in data {
+        res = res * 10 + ascii_to_digit(ch)?;
+    }
+
+    Ok(res)
+}
+
+#[test]
+fn test_ascii_slice_to_u32() {
+    let d: Vec<u8> = "12345".bytes().collect();
+    assert_eq!(12345, ascii_slice_to_u32(&d).unwrap());
+}
+
 impl<'a> Display for UTCTime<'a> {
     fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
-        if let Ok(s) = String::from_utf8(self.0.to_vec()) {
-            write!(f, "{}", s)
+        if let Ok(dt) = self.to_datetime() {
+            write!(f, "{}", dt.to_rfc3339())
         } else {
             Err(fmt::Error::default())
         }
@@ -207,7 +270,7 @@ enum Value<'a> {
     ObjectIdentifier(ObjectIdentifier<'a>),
     Sequence(Vec<Value<'a>>),
     //TODO: use different type
-    UTCTime(String),
+    UTCTime(UTCTime<'a>),
     //TODO: use different type
     GeneralizedTime(String),
     PrintableString(PrintableString<'a>),
@@ -303,11 +366,7 @@ fn parse_set(data: &[u8]) -> Result<Value, Error> {
 }
 
 fn parse_utc_time(data: &[u8]) -> Result<Value, Error> {
-    if let Ok(s) = String::from_utf8(data.to_vec()) {
-        Ok(Value::UTCTime(s))
-    } else {
-        Err(Error::ParseError)
-    }
+    Ok(Value::UTCTime(UTCTime(data)))
 }
 
 fn parse_generalized_time(data: &[u8]) -> Result<Value, Error> {
